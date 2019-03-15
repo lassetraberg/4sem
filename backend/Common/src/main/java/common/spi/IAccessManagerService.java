24ab8160b7634b6518cdff4a2665998a07df5672
bjarkt <bjarke.tobiesen@gmail.com>
2019-03-15 21:20:23 +0100

    docker

---
+++ backend/Common/src/main/java/common/spi/IAccessManagerService.java
@@ -0,0 +1,11 @@
+package common.spi.accessManagerService;
+
+import io.javalin.Context;
+import io.javalin.Handler;
+import io.javalin.security.Role;
+
+import java.util.Set;
+
+public interface IAccessManagerService {
+    void configure(Handler handler, Context ctx, Set<Role> permittedRoles) throws Exception;
+}
--- backend/Common/src/main/java/common/spi/IConfigurationService.java
+++
@@ -1,7 +0,0 @@
-package common.spi;
-
-import io.javalin.Javalin;
-
-public interface IConfigurationService {
-    void configure(Javalin app);
-}
