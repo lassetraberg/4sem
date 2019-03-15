77f936106f3b121040d595e08c2fa71424ba1afd
Lasse Traberg-Larsen <lasse@traberg-larsen.dk>
2019-03-15 21:56:17 +0100

    small change in router hierarchy

--- frontend/src/app/modules/auth/auth-routing.module.ts
+++ frontend/src/app/modules/auth/auth-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
     {
-        path: '',
+        path: 'signin',
         component: SigninComponent,
         data: {title: "SignIn"}
+    },
+    {
+        path: 'signup',
+        component: SignupComponent,
+        data: {title: "Register",}
     }
 ];
 
