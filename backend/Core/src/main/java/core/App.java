b70643b7eeabeedf900baac89c8801379207c956
bjarkt <bjarke.tobiesen@gmail.com>
2019-03-15 17:16:44 +0100

    make /routes work again, mvn package

--- backend/Core/src/main/java/core/App.java
+++ backend/Core/src/main/java/core/App.java
@@ -4,7 +4,7 @@ import core.config.ConfigFactory;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class App {
-    public static void main(String[] args) throws MqttException {
+    public static void main(String[] args) {
         ConfigFactory configFactory = new ConfigFactory();
         configFactory.getAppConfig().setup().start();
     }
