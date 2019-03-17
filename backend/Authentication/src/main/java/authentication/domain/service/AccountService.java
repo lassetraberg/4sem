158ec81dafa1035a0df8dd724a830a10e1727ba8
bjarkt <bjarke.tobiesen@gmail.com>
2019-03-17 16:45:37 +0100

    use bcrypt for password hashing+salt

--- backend/Authentication/src/main/java/authentication/domain/service/AccountService.java
+++ backend/Authentication/src/main/java/authentication/domain/service/AccountService.java
@@ -1,22 +1,20 @@
 package authentication.domain.service;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import authentication.domain.Account;
 import authentication.domain.repository.AccountRepository;
-import authentication.util.Cipher;
 import authentication.util.JwtProvider;
 import commonAuthentication.config.authConfig.Roles;
 import io.javalin.HttpResponseException;
 import io.javalin.UnauthorizedResponse;
 import org.eclipse.jetty.http.HttpStatus;
 
-import java.util.Base64;
 import java.util.Collections;
 import java.util.Map;
 
 public class AccountService {
     private AccountRepository accountRepository;
     private JwtProvider jwtProvider;
-    private java.util.Base64.Encoder base64Encoder = Base64.getEncoder();
 
     public AccountService(AccountRepository accountRepository, JwtProvider jwtProvider) {
         this.accountRepository = accountRepository;
@@ -30,7 +28,7 @@ public class AccountService {
             throw new HttpResponseException(HttpStatus.BAD_REQUEST_400,
                     "Username already registered", details);
         } else {
-            String hashedPassword = new String(base64Encoder.encode(Cipher.encrypt(account.getPassword())));
+            String hashedPassword = generateHashedPassword(account.getPassword());
             account.setPassword(hashedPassword);
             account.setToken(generateJwtToken(account));
             accountRepository.createUser(account);
@@ -40,8 +38,11 @@ public class AccountService {
 
     public Account authenticate(Account account) {
         Account existingAccount = accountRepository.findByUsername(account.getUsername());
-        String hashedPassword = new String(base64Encoder.encode(Cipher.encrypt(account.getPassword())));
-        if (existingAccount != null && existingAccount.getPassword().equals(hashedPassword)) {
+        if (existingAccount == null) {
+            throw new UnauthorizedResponse("Invalid username or password!");
+        }
+
+        if (isPasswordCorrect(account.getPassword(), existingAccount.getPassword())) {
             existingAccount.setToken(generateJwtToken(account));
             return existingAccount;
         } else {
@@ -51,5 +52,13 @@ public class AccountService {
 
     private String generateJwtToken(Account account) {
         return jwtProvider.createJWT(account, Roles.AUTHENTICATED);
+    }
+
+    private String generateHashedPassword(String plaintextPassword) {
+        return BCrypt.withDefaults().hashToString(12, plaintextPassword.toCharArray());
+    }
+
+    private boolean isPasswordCorrect(String plaintextPassword, String hashedPassword) {
+        return BCrypt.verifyer().verify(plaintextPassword.toCharArray(), hashedPassword).verified;
     }
 }
