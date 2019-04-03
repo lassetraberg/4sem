3558be276a3062f63ffeb2775262560df61e3042
bjarkt <bjarke.tobiesen@gmail.com>
2019-04-03 18:04:29 +0200

    predefined gps coordinates

--- backend/Simulation/src/main/java/simulation/hardware/sensors/GPSSensor.java
+++ backend/Simulation/src/main/java/simulation/hardware/sensors/GPSSensor.java
@@ -5,23 +5,47 @@ import common.spi.IMqttService;
 import simulation.hardware.AbstractDevice;
 import simulation.hardware.State;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 public class GPSSensor extends AbstractDevice {
 
-    public GPSSensor(IMqttService client) {
+    private List<Double[]> fileData;
+
+    private int dataIndex = 0;
+    private boolean reverse = false;
+
+    public GPSSensor(IMqttService client, String gpsRouteFileName) {
         super(client);
+        if (gpsRouteFileName != null) {
+            this.fileData = readFileData(gpsRouteFileName);
+        } else {
+            this.fileData = null;
+        }
+    }
+
+    public GPSSensor(IMqttService client) {
+        this(client, null);
     }
 
     public void call(State state) {
-        client.publish(VariableMqttTopic.VEHICLE_GPS, state.getDeviceId(), toJson(generateGpsLatLon()));
+        if (fileData != null) {
+            client.publish(VariableMqttTopic.VEHICLE_GPS, state.getDeviceId(), toJson(getPredefinedGpsLatLon()));
+        } else {
+            client.publish(VariableMqttTopic.VEHICLE_GPS, state.getDeviceId(), toJson(generateRandomGpsLatLon()));
+        }
     }
 
     private String toJson(double[] gpsLatLon) {
         return String.format("{ \"lat\": %f, \"lon\": %f }", gpsLatLon[0], gpsLatLon[1]);
     }
 
-    private double[] generateGpsLatLon() {
+    private double[] generateRandomGpsLatLon() {
         double lat = 55.3733;
         double lon = 10.4303;
 
@@ -29,5 +53,37 @@ public class GPSSensor extends AbstractDevice {
         lon = ThreadLocalRandom.current().nextDouble(lon - 2, lon + 2);
 
         return new double[]{lat, lon};
+    }
+
+    private double[] getPredefinedGpsLatLon() {
+        Double[] latlon = fileData.get(dataIndex);
+        if (dataIndex == fileData.size()-1) {
+            reverse = true;
+        } else if (dataIndex == 0) {
+            reverse = false;
+        }
+        if (reverse) {
+            dataIndex--;
+        } else {
+            dataIndex++;
+        }
+
+        return new double[]{latlon[0], latlon[1]};
+    }
+
+    private List<Double[]> readFileData(String gpsRouteFileName) {
+        List<Double[]> list = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/simulation/gpsroutes/"+gpsRouteFileName), StandardCharsets.UTF_8))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("#")) continue;
+                Double[] latlon = Arrays.stream(line.split(",")).map(it -> Double.parseDouble(it.trim())).collect(Collectors.toList()).toArray(new Double[]{});
+                list.add(latlon);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return list;
     }
 }
