8984de95948cad90b06b9a58929567e61e2679a4
Lasse Traberg-Larsen <lasse@traberg-larsen.dk>
2019-04-05 12:35:40 +0200

    http interceptor reacts if token expired
    user gets redirected to login screen.

--- frontend/src/app/shared/interceptors/AuthInterceptor.ts
+++ frontend/src/app/shared/interceptors/AuthInterceptor.ts
@@ -1,25 +1,46 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError, tap} from 'rxjs/operators';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-    intercept(req: HttpRequest<any>,
-              next: HttpHandler): Observable<HttpEvent<any>> {
+    constructor(private router: Router) { }
 
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         const idToken = localStorage.getItem("id_token");
+        var request;
 
         if (idToken) {
-            const cloned = req.clone({
+            request = req.clone({
                 headers: req.headers.set("Authorization",
                     "Bearer " + idToken)
             });
+        } else {
+            request = req;
+        }
 
-            return next.handle(cloned);
-        }
-        else {
-            return next.handle(req);
-        }
+        return next.handle(request).pipe(
+            tap((event: HttpEvent<any>) => {
+                if(event instanceof HttpResponse){
+                    
+                }
+            }, (err: any) => {
+                if(err instanceof HttpErrorResponse){
+                    if(err.status === 401 || err.status === 403){
+                        this.router.navigate(['auth/signin']);
+                    }
+                }
+            })
+        );
     }
 }
+
+
+
+
+
+
