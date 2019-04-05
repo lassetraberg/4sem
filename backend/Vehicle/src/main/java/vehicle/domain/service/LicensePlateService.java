f2bfe54f4fbeed51eec0e8cba86321574f3e0144
Dennis Petersen <Dennis-drengen@hotmail.dk>
2019-04-05 12:26:49 +0200

    Added DataCache for minimising api calls

--- backend/Vehicle/src/main/java/vehicle/domain/service/LicensePlateService.java
+++ backend/Vehicle/src/main/java/vehicle/domain/service/LicensePlateService.java
@@ -7,24 +7,40 @@ import com.mashape.unirest.request.GetRequest;
 import common.config.Config;
 import io.javalin.NotFoundResponse;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LicensePlateService implements ILicensePlateService{
+    private Map<String, String> dataCache;
+
+    public LicensePlateService() {
+        this.dataCache = new HashMap<>();
+    }
 
     @Override
     public String getData(String licensePlate) {
-        GetRequest getRequest = Unirest.get("https://v1.motorapi.dk/vehicles/" + licensePlate);
-        getRequest.header("X-AUTH-TOKEN", Config.getInstance().getProperty("motor.apikey"));
+        String data = "";
 
-        try {
-            HttpResponse<String> httpResponse = getRequest.asString();
-            if(httpResponse.getStatus() == 404) {
-                throw new NotFoundResponse(httpResponse.getStatusText());
+        if(dataCache.containsKey(licensePlate)) {
+            data = dataCache.get(licensePlate);
+        } else {
+
+            GetRequest getRequest = Unirest.get("https://v1.motorapi.dk/vehicles/" + licensePlate);
+            getRequest.header("X-AUTH-TOKEN", Config.getInstance().getProperty("motor.apikey"));
+
+            try {
+                HttpResponse<String> httpResponse = getRequest.asString();
+                if(httpResponse.getStatus() == 404) {
+                    throw new NotFoundResponse(httpResponse.getStatusText());
+                }
+
+                data = httpResponse.getBody();
+                dataCache.put(licensePlate, data);
+            } catch (UnirestException e) {
+                e.printStackTrace();
             }
-
-            return httpResponse.getBody();
-        } catch (UnirestException e) {
-            e.printStackTrace();
         }
 
-        return "";
+        return data;
     }
 }
