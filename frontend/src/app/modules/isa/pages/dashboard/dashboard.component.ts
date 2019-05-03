b32c404b8c22f2252b72306cd62ae8d77c9e7960
bjarkt <bjarke.tobiesen@gmail.com>
2019-05-04 00:54:17 +0200

    and remove hardcoded device id in isa

--- frontend/src/app/modules/isa/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/isa/pages/dashboard/dashboard.component.ts
@@ -4,6 +4,8 @@ import { WebSocketSubject } from 'rxjs/webSocket';
 import { VehicleData } from 'src/app/shared/models/vehicledata';
 import { interval, Subscription, Observable, of, BehaviorSubject } from 'rxjs';
 import { MapComponent } from 'src/app/shared/components/map/map.component';
+import { DataService } from 'src/app/shared/services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -20,14 +22,23 @@ export class DashboardComponent implements OnInit {
 
   vehicleData: VehicleData;
 
+  deviceId: string = null;
+
   sockets = {};
 
-  constructor(private socket: WebsocketService) { 
+  constructor(private socket: WebsocketService, private data: DataService, private router: Router) { 
     this.vehicleData = new VehicleData();
   }
 
   ngOnInit() {
     // Connect to WebSockets for the properties of VehicleData.   
+    const myVehicle = this.data.getMyVehicle()
+    if (myVehicle === null) {
+      this.router.navigate(["user/"]);
+    } else {
+      this.deviceId = myVehicle.deviceId;
+    }
+
     this.subscribeToWebSockets();
   }
 
@@ -39,7 +50,7 @@ export class DashboardComponent implements OnInit {
     this.connecting = true;
     this.unsubscribeWebSockets();
     Object.keys(this.vehicleData).forEach(key => {  
-      this.sockets[key] = this.socket.getSubject("2905d0e7-615c-455b-8807-ddd7665d3994", key);
+      this.sockets[key] = this.socket.getSubject(this.deviceId, key);
       this.sockets[key].subscribe(
         msg => {
           this.connected = true; 
