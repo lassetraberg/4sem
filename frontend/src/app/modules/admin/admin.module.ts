bbd1845d3e985926395bc036673596b05448c93b
bjarkt <bjarke.tobiesen@gmail.com>
2019-05-04 16:10:34 +0200

    replay vehicle data

--- frontend/src/app/modules/admin/admin.module.ts
+++ frontend/src/app/modules/admin/admin.module.ts
@@ -8,6 +8,7 @@ import { AdminRoutingModule } from "./admin-routing.module";
 import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 import { UsersComponent } from "./pages/users/users.component";
 import { DataComponent } from "./pages/data/data.component";
+import { SharedModule } from "src/app/shared/components/shared.module";
 @NgModule({
   declarations: [DashboardComponent, UsersComponent, DataComponent],
   imports: [
@@ -16,7 +17,8 @@ import { DataComponent } from "./pages/data/data.component";
     MatExpansionModule,
     MatTableModule,
     MatPaginatorModule,
-    MatCardModule
+    MatCardModule,
+    SharedModule
   ],
   providers: []
 })
--- frontend/src/app/modules/admin/pages/data/data.component.ts
+++ frontend/src/app/modules/admin/pages/data/data.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { DataService } from "src/app/shared/services/data.service";
+import { VehicleData } from "src/app/shared/models/vehicledata";
+import { MapComponent } from "src/app/shared/components/map/map.component";
 
 @Component({
   selector: "app-data",
@@ -15,6 +17,8 @@ export class DataComponent implements OnInit {
     "speedLimit",
     "acceleration"
   ];
+  @ViewChild(MapComponent) map;
+  showMap: any = {};
 
   constructor(private data: DataService) {}
 
@@ -31,6 +35,48 @@ export class DataComponent implements OnInit {
     return Object.keys(this.vehicleData).length > 0;
   }
 
+  replayData(data: Array<VehicleData>) {
+    if (data.length > 0) {
+      this.showMap[data[0].deviceId] = true;
+    }
+    const loop = i => {
+      if (i === data.length - 1) return;
+      const currentTimestamp = new Date(data[i].timestamp);
+      const nextTimestamp = new Date(data[i + 1].timestamp);
+      const diffInMS = nextTimestamp.getTime() - currentTimestamp.getTime();
+
+      if (this.map) {
+        this.map.updateMap(data[i].gps);
+        const velocityElem = document.getElementById(
+          `${data[i].deviceId}-velocity`
+        );
+        const speedLimitElem = document.getElementById(
+          `${data[i].deviceId}-speedLimit`
+        );
+        velocityElem.innerText = " " + data[i].velocity + " ";
+        speedLimitElem.innerText = " " + data[i].speedLimit + " ";
+
+        if (data[i].velocity > data[i].speedLimit) {
+          velocityElem.classList.add("speeding");
+        } else {
+          velocityElem.classList.remove("speeding");
+        }
+      }
+
+      setTimeout(() => {
+        loop(i + 1);
+      }, diffInMS);
+    };
+    loop(0);
+  }
+
+  stopReplay(data: Array<VehicleData>) {
+    if (data.length > 0) {
+      this.showMap[data[0].deviceId] = false;
+    }
+    // TODO figure out how to stop the setTimeout
+  }
+
   private getVehicleData() {
     this.data
       .getAllData()
@@ -38,6 +84,7 @@ export class DataComponent implements OnInit {
       .then(vehicleData => {
         this.vehicleData = vehicleData.reduce((results, curr) => {
           (results[curr.deviceId] = results[curr.deviceId] || []).push(curr);
+          this.showMap[curr.deviceId] = false;
           return results;
         }, {});
       });
